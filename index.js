08b29a05554b4af2220b501daddcf51cbbe1db0d
bonasunu <bonaventurasunu@gmail.com>
2020-08-07 14:37:48 +0700

    build routes for get and post method

--- index.js
+++ index.js
@@ -4,8 +4,28 @@ const pool = require('./db')
 
 app.use(express.json())
 
+app.get("/api/todos", async (req, res) => {
+  const allTodos = await pool.query(
+    "SELECT * FROM todo"
+  )
 
+  res.json(allTodos.rows)
+})
 
-app.listen(8000, () => {
-  console.log('App listening on port 8000')
+app.post("/api/todos", async (req, res) => {
+  try {
+    await console.log(req.body)
+    const { description } = req.body
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES ($1) RETURNING *", [description]
+    )
+
+    res.json(newTodo)
+  } catch(err) {
+    console.log(err.message)
+  }
+})
+
+app.listen(5000, () => {
+  console.log('App listening on port 5000')
 })
