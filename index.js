d4238390984e6967ecadb6cba3fcffb13c50de55
bonasunu <bonaventurasunu@gmail.com>
2020-08-07 13:43:24 +0700

    create db.js to connect database

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express')
 const app = express()
+const pool = require('./db')
+
+app.use(express.json())
+
+
 
 app.listen(8000, () => {
   console.log('App listening on port 8000')
