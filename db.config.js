98a4f4df1e560f647a8efc4f249889432caa3bd5
bonasunu <bonaventurasunu@gmail.com>
2020-08-09 18:18:22 +0700

    edit pg pool setting inside db.config

--- db.config.js
+++ db.config.js
@@ -1,18 +1,5 @@
 const Pool = require('pg-pool')
-const config = require('./utils/config')
 
-const USER = config.USER
-const PASSWORD = config.PASSWORD
-const DATABASE = config.DATABASE
-const HOST = config.HOST
-const PGPORT = config.PGPORT
-
-const pool = new Pool({
-  user: USER,
-  password: PASSWORD,
-  database: DATABASE,
-  host: HOST,
-  port: PGPORT
-})
+const pool = new Pool()
 
 module.exports = pool
