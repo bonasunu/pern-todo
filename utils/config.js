6daf4ead1ec4448be2dec13488af82e4f355867e
bonasunu <bonaventurasunu@gmail.com>
2020-08-09 18:19:35 +0700

    delete pg config inside config.js

--- utils/config.js
+++ utils/config.js
@@ -1,17 +1,7 @@
 require('dotenv').config()
 
-const USER = process.env.PGUSER
-const PASSWORD = process.env.PGPASSWORD
-const DATABASE = process.env.PGDATABASE
 const PORT = process.env.PORT
-const HOST = process.env.PGHOST
-const PGPORT = process.env.PGPORT
 
 module.exports = {
-  USER,
-  PASSWORD,
-  DATABASE,
   PORT,
-  PGPORT,
-  HOST
 }
